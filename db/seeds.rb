81d2c61ed37ee0fd81cc6d06e8032365f7e908f7
Alexandre Vrla <alexandre.vrla@gmail.com>
2021-12-12 11:19:25 +0100

    cars seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,5 +5,23 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+if Rails.env.development?
+  puts "Cleaning database..."
+  Car.destroy_all
+end
 
-cars = Car.create([{ brand: 'Lexus' }, { model: 'S'}, { price_per_day: 95}, { current_location: 'Atlanta'}, { rental_status: 'available'}])
+puts "Creating DB..."
+puts "Creating Cars..."
+
+Lexus_LS = Car.create({ brand: 'Lexus', model: 'LS', price_per_day: 95, current_location: 'Atlanta', rental_status: 'available'})
+# Lexus_LS.save!
+Renault_Espace = Car.create({ brand: 'Renault', model: 'Espace', price_per_day: 95, current_location: 'Cleveland', rental_status: 'available'})
+# Renault_Espace.save!
+Cadillac_Escalade = Car.create({ brand: 'Cadillac', model: 'Escalade', price_per_day: 135, current_location: 'Detroit', rental_status: 'available'})
+# Cadillac_Escalade.save!
+Cadillac_Lowrider = Car.create({ brand: 'Cadillac', model: 'Lowrider', price_per_day: 135, current_location: 'Los Angeles', rental_status: 'available'})
+# Cadillac_Lowrider.save!
+Fiat_Punto = Car.create({ brand: 'Fiat', model: 'Punto', price_per_day: 65, current_location: 'Seattle', rental_status: 'rented'})
+# Fiat_Punto.save!
+
+puts "Cars added!"
