fd479d043a21c1de135c40b6e28cbe4c8a834800
Alexandre Vrla <alexandre.vrla@gmail.com>
2021-12-12 17:43:20 +0100

    silo

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,6 +1,6 @@
 class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :car
-  belongs_to :departure, :class_name => "Location", :foreign_key => 'departure_location'
-  belongs_to :arrival, :class_name => "Location", :foreign_key => 'arrival_location'
+  belongs_to :departure, class_name: "Location", foreign_key: 'departure_location'
+  belongs_to :arrival, class_name: "Location", foreign_key: 'arrival_location'
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: 'pages#home'
+  root to: 'bookings#new'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :bookings, only: %i[new create edit update show] do
+    resources :cars, only: %i[index show]
+  end
 end
